e3b5978040f97b502b7127696e545c341e1cf495
Dustin Hiatt <dustin.hiatt@rkinetic.com>
2017-04-25 13:08:20 -0500

    Add version to setup.

--- src/setup.py
+++ src/setup.py
@@ -5,12 +5,12 @@ from distutils.core import setup
 setup(
     name = 'cloud_ftp',
     packages = ['cloud_ftp'],
-    version = '0.1',
+    version = 'v1.0.0',
     description = 'Convenience wrapper for downloading FTP files on Google App Engine',
     author = 'Real Kinetic',
     author_email = 'dev@realkinetic.com',
     url = 'https://github.com/RealKinetic/cloud-ftp',
-    download_url = 'download link you saved',
+    download_url = 'https://github.com/RealKinetic/cloud-ftp/archive/v1.0.0.tar.gz',
     keywords = ['app engine', 'ftp'],
     classifiers = [],
 )
