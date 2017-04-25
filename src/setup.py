aeb79c15b375e063e5d3b80a73635b13a31d5291
Dustin Hiatt <dustin.hiatt@rkinetic.com>
2017-04-25 08:49:51 -0500

    Add required pypi files.

---
+++ src/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'cloud_ftp',
+    packages = ['cloud_ftp'],
+    version = '0.1',
+    description = 'Convenience wrapper for downloading FTP files on Google App Engine',
+    author = 'Real Kinetic',
+    author_email = 'dev@realkinetic.com',
+    url = 'https://github.com/RealKinetic/cloud-ftp',
+    download_url = 'download link you saved',
+    keywords = ['app engine', 'ftp'],
+    classifiers = [],
+)
