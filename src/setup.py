5cdca40485502b19acd95ad7aebb2f3b43a78291
Dustin Hiatt <dustin.hiatt@rkinetic.com>
2017-04-26 15:31:47 -0500

    Modify setup for release.

--- src/setup.py
+++ src/setup.py
@@ -6,12 +6,12 @@ from setuptools import find_packages
 setup(
     name = 'cloud_ftp',
     packages = find_packages(),
-    version = 'v1.0.0',
+    version = 'v1.0.1',
     description = 'Convenience wrapper for downloading FTP files on Google App Engine',
     author = 'Real Kinetic',
     author_email = 'dev@realkinetic.com',
     url = 'https://github.com/RealKinetic/cloud-ftp',
-    download_url = 'https://github.com/RealKinetic/cloud-ftp/archive/v1.0.0.tar.gz',
+    download_url = 'https://github.com/RealKinetic/cloud-ftp/archive/v1.0.1.tar.gz',
     keywords = ['app engine', 'ftp'],
     classifiers = [],
 )
